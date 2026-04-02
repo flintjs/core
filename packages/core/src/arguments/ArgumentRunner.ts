53d098d6dea545d0799b01a949df03e4ded86e85
Brad <44570552+bizojs@users.noreply.github.com>
2026-04-02 16:05:09 +0100

    fix: argument flags and options

--- packages/core/src/arguments/ArgumentRunner.ts
+++ packages/core/src/arguments/ArgumentRunner.ts
@@ -15,33 +15,68 @@ export class ArgumentRunner {
 
         const args = argDefinitions.map(def => new Argument(def))
         const result: Record<string, unknown> = {}
+        const usedIndexes = new Set<number>()
+
+        for (const arg of args) {
+            switch (arg.match) {
+                case "flag": {
+                    const flags = arg.flag ?? []
+
+                    let found = false
+
+                    rawArgs.forEach((a, i) => {
+                        if (flags.includes(a)) {
+                            usedIndexes.add(i)
+                            found = true
+                        }
+                    })
+
+                    result[arg.id] = found
+                    break
+                }
+                case "option": {
+                    const flags = arg.flag ?? []
+                    let value: unknown = arg.resolveDefault(client, message)
+
+                    rawArgs.forEach((a, i) => {
+                        if (a.includes("=")) {
+                            const [flag, val] = a.split("=")
+                            if (flags.includes(flag)) {
+                                usedIndexes.add(i)
+                                value = val
+                            }
+                        } else if (flags.includes(a) && rawArgs[i + 1]) {
+                            usedIndexes.add(i)
+                            usedIndexes.add(i + 1)
+
+                            value = rawArgs[i + 1]
+                        }
+                    })
+
+                    const resolved = await this.#cast(arg, client, message, value as string)
+                    result[arg.id] = resolved ?? arg.resolveDefault(client, message)
+                    break
+                }
+            }
+        }
+
+        const cleanArgs = rawArgs.filter((_, i) => !usedIndexes.has(i))
         let phraseIndex = 0
 
         for (const arg of args) {
             switch (arg.match) {
                 case "phrase": {
-                    const phrase = rawArgs[phraseIndex++] ?? ""
+                    const phrase = cleanArgs[phraseIndex++] ?? ""
                     const resolved = await this.#cast(arg, client, message, phrase)
                     result[arg.id] = resolved ?? arg.resolveDefault(client, message)
                     break
                 }
+
                 case "rest": {
-                    const phrase = rawArgs.slice(phraseIndex).join(" ")
-                    phraseIndex = rawArgs.length
+                    const phrase = cleanArgs.slice(phraseIndex).join(" ")
+                    phraseIndex = cleanArgs.length
                     const resolved = await this.#cast(arg, client, message, phrase)
                     result[arg.id] = resolved ?? arg.resolveDefault(client, message)
-                    break
-                }
-                case "flag": {
-                    const flags = arg.flag ?? []
-                    const flagIndex = rawArgs.findIndex(a => flags.includes(a))
-                    if (flagIndex !== -1 && rawArgs[flagIndex + 1]) {
-                        const phrase = rawArgs[flagIndex + 1]
-                        const resolved = await this.#cast(arg, client, message, phrase)
-                        result[arg.id] = resolved ?? arg.resolveDefault(client, message)
-                    } else {
-                        result[arg.id] = arg.resolveDefault(client, message)
-                    }
                     break
                 }
             }
